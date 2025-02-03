71d74fca9df06e46145b0bb31eb2a180b70e23db
BeldeSahan <74128115+BeldeSahan@users.noreply.github.com>
2025-02-03 19:57:00 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -32,7 +32,7 @@ function draw() {
 }
 function keyPressed(){
   if( keyCode === UP_ARROW){
-  Matter.Body.applyForce(paper.body,paper.body.position,{x:21,y:-33})
+  Matter.Body.applyForce(paper.body,paper.body.position,{x:21,y:-10})
   }
   }
   
